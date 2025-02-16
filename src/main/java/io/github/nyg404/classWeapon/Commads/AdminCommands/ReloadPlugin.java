0347d1928be7b264e232233e607661380a5d1180
Nyg404 <karrr213@Mail.ru>
2025-02-16 13:31:29 +0500

    sdsadsd

--- src/main/java/io/github/nyg404/classWeapon/Commads/AdminCommands/ReloadPlugin.java
+++ src/main/java/io/github/nyg404/classWeapon/Commads/AdminCommands/ReloadPlugin.java
@@ -14,6 +14,7 @@ public class ReloadPlugin implements CommandExecutor {
         this.plugin = plugin;
     }
 
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender.hasPermission("classweapon.reload")) { // Проверка на права
